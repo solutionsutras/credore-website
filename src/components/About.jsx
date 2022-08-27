7b7f99b699df63d079b09da05ba95fe07d6c749b
jitenvaidya <67831330+jitenvaidya@users.noreply.github.com>
2022-08-27 11:46:37 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div id="about" className="mx-auto container text-center py-28">
-      <h3 className="text-6xl font-bold my-10">About Credore-----</h3>
+      <h3 className="text-6xl font-bold my-10">About Credore</h3>
       <p className="my-10 text-xl mb-28">
         Credore is a protocol providing crypto investors an avenue to invest in
         real world business activities and earn non-speculative yields. At the
