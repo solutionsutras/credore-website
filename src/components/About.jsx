af0ffe8f1474bea97cc4316de8d0fc63e1c865b6
jitenvaidya <67831330+jitenvaidya@users.noreply.github.com>
2022-08-27 13:50:19 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
       <h3 className="text-6xl font-bold my-10">Why Credore?</h3>
       <p className="my-10 text-xl mb-28">
         Global trade is the backbone of the world economy. However, trading from ancient times is highly dependent on paper documents, in spite of decades long efforts to digitalize. A cross-border transaction involves multiple actors and on average requires the exchange of 36 documents and 240 copies. Currently, fewer than one percent of trade documents are fully digitized.
- </p> 
-    <p className="my-10 text-xl mb-28">
+ <br />
+    
     The lack of adoption of data standards, data privacy and on how trade data are exchanged, combined with an actual absence of standards, significantly hinder the seamless data flow from one end of the supply chain to the other. This has diminished efficiency gains along trade finance and documents exchange and has weighed heavily on companies, in particular small businesses.
 
       </p>
