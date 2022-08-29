fa93535bee16b7200267b83abf265815b76f6d6b
jitenvaidya <67831330+jitenvaidya@users.noreply.github.com>
2022-08-29 17:44:44 +0530

    Update team.jsx

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -3,8 +3,7 @@ const Team = () => {
              
            <picture>
      
-        <source srcSet="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png" type="image/webp" />
-         <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png" alt="Landscape picture" />
+                <img src="/assets/images/trade_zkcredore.svg" alt="" className="w-full" />
       </picture>
   </picture>
   );
