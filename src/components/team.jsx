bd3ae0333516cbd86d46d58339577438b62f38c0
jitenvaidya <67831330+jitenvaidya@users.noreply.github.com>
2022-08-29 17:46:27 +0530

    Update team.jsx

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -5,7 +5,7 @@ const Team = () => {
      
                 <img src="/assets/images/trade_zkcredore.svg" alt="" className="w-full" />
       </picture>
-  </picture>
+  
   );
 };
 export default Team; 
