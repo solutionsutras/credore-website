f9571b4e0488c6ca1e3075b9727f3fd413bbce36
jitenvaidya <67831330+jitenvaidya@users.noreply.github.com>
2022-08-29 18:34:53 +0530

    Update team.jsx

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -150,7 +150,7 @@ const Team = () => {
                       </li>
                   </ul>
               </div>
-     <div classname="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+     <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
           </div>
     
     
