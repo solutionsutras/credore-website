c9d6b64fc2c483e14971a81a07b22c51f126ae21
solutionsutras <solutionsutras@gmail.com>
2023-08-10 13:11:13 +0530

    20230810

--- src/components/SupportedBy.jsx
+++ src/components/SupportedBy.jsx
@@ -7,36 +7,36 @@ const SupportedBy = () => {
   return (
     <section className="section">
       <div className="container">
-        <div className="flex flex-row gap-4">
-          <div className="p-5 pl-0 pb-10 flex flex-col gap-2 justify-between items-center">
+        <div className="flex flex-row gap-4 items-center">
+          <div className="p-5 pl-0 pb-10 flex flex-col gap-1">
             <h1 className="text-lg text-left">Incubated By</h1>
             <Image
               className=""
               src={"/assets/images/partners/incubated-by-padup-ventures.png"}
-              width={"100%"}
-              height={"100%"}
+              width={"272"}
+              height={"140"}
               alt=""
             />
           </div>
 
-          <div className="p-5 pl-0 pb-10 flex flex-col gap-2">
+          <div className="p-5 pl-0 pb-10 flex flex-col gap-1">
             <h1 className="text-lg text-left">Backed By</h1>
             <Image
               className=""
               src={"/assets/images/partners/backed-by-stpi-next.png"}
               alt=""
-              width={"100%"}
-              height={"100%"}
+              width={"660"}
+              height={"140"}
             />
           </div>
 
-          <div className="p-5 pl-0 pb-10 flex flex-col gap-2">
+          <div className="p-5 pl-0 pb-10 flex flex-col gap-1">
             <h1 className="text-lg text-left">Collaborations</h1>
             <Image
               className=""
               src={"/assets/images/partners/edge.png"}
               alt=""
-              width={"100%"}
+              width={"296"}
               height={"100%"}
             />
           </div>
